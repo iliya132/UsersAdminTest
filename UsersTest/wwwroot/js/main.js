9914296964b5cb5f2b2400154f5b4698bb677f0e
Ilya132@live.com <iliya132@live.com>
2020-11-27 15:02:47 +0300

    WIP3

--- UsersTest/wwwroot/js/main.js
+++ UsersTest/wwwroot/js/main.js
@@ -4,7 +4,16 @@ let connection = new XMLHttpRequest();
 let users = [];
 
 function EditUser(user) {
-    alert('Edited ' + user.name);
+    let nameField = document.getElementById('modalNameField');
+    let emailField = document.getElementById('modalEmailField');
+    user.name = nameField.value;
+    user.email = emailField.value;
+    Refresh();
+}
+
+function DeleteUser(user) {
+    users.splice(users.findIndex(v => v == user), 1);
+    Refresh();
 }
 
 function CreateElement(type, innerHtml, owner) {
@@ -14,48 +23,55 @@ function CreateElement(type, innerHtml, owner) {
     return newElem;
 }
 
+function Refresh() {
+    let tableBody = document.getElementById("UserRows");
+    tableBody.innerHTML = '';
+    users.forEach(function (itm, i, arr) {
+        let newRow = document.createElement("tr");
+        CreateElement("th", itm.id, newRow);
+        CreateElement("th", itm.login, newRow);
+        CreateElement("th", itm.name, newRow);
+        CreateElement("th", itm.email, newRow);
+        let roles = itm.roles.map(function (rl) {
+            return rl.name;
+        }).join(', ');
+        CreateElement("th", roles, newRow);
+        let editBtn = document.createElement("button");
+        editBtn.setAttribute("class", "btn btn-secondary mr-2");
+        editBtn.appendChild(document.createTextNode("Edit"));
+        newRow.appendChild(editBtn);
+        editBtn.onclick = function () {
+
+            $("#editModalWindow").modal('show');
+            document.getElementById('modalNameField').value = users[i].name;
+            document.getElementById('modalEmailField').value = users[i].email;
+            ConfirmEdit.onclick = function () { EditUser(users[i]); }
+            $("#editModalWindow").modal('hide');
+        }
+        let deleteBtn = document.createElement("button");
+        deleteBtn.setAttribute("class", "btn btn-danger");
+
+        deleteBtn.appendChild(document.createTextNode("Delete"));
+        newRow.appendChild(deleteBtn);
+        deleteBtn.onclick = function () {
+            DeleteUser(users[i]);
+            
+        }
+        tableBody.appendChild(newRow);
+    });
+}
+
 window.onload = function(){
     connection.open('GET', 'https://localhost:44393/Users/allusers', true);
-    
     connection.onreadystatechange = function(){
         if(connection.readyState == 4){
-            
             if(connection.status == 200){
-
-            users = JSON.parse(connection.responseText);
-            users.forEach(function(itm, i, arr){
-                let tableBody = document.getElementById("UserRows");
-                let newRow = document.createElement("tr");
-                CreateElement("th", itm.id, newRow);
-                CreateElement("th", itm.login, newRow);
-                CreateElement("th", itm.name, newRow);
-                CreateElement("th", itm.email, newRow);
-                let roles = itm.roles.map(function (rl) {
-                    return rl.name;
-                }).join(', ');
-                CreateElement("th", roles, newRow);
-                let editBtn = document.createElement("button");
-                editBtn.setAttribute("class", "btn btn-primary");
-                editBtn.appendChild(document.createTextNode("Edit"));
-                newRow.appendChild(editBtn);
-                editBtn.onclick = function () {
-                    EditUser(users[i]);
-                    tableBody.removeChild(newRow);
-                }
-                let deleteBtn = document.createElement("button");
-                deleteBtn.setAttribute("class", "btn btn-primary");
-                deleteBtn.appendChild(document.createTextNode("Delete"));
-                newRow.appendChild(deleteBtn);
-                
-                tableBody.appendChild(newRow);
-            });
+                users = JSON.parse(connection.responseText);
+                Refresh();
         }
     }
     }
-    
     connection.send();
-    
-    
 }
 
 
